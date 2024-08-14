30c4a0906eb88e7b8ec1295d9fdcd2ef1577322a
Emy <141051806+YuiMochiMochi@users.noreply.github.com>
2024-08-14 12:37:18 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <section>
+      <div>
+        <header>
+
+        </header>
+      </div>
+      <div>
+        <h1>Jogos Olimpicos</h1>
+        <h2>Um evento mundialmento conhecido e frequentado.</h2>
+      </div>
+      <div>
+        
+      </div>
+    </section>
+  )
+}
+
+export default App
